51272061c732ac2f1dd2b5dd1e9683577e03e174
natalielh <nlehuenen@gmail.com>
2020-07-29 17:37:42 -0400

    demo with character

---
+++ Assets/playerFlip2.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerFlip2 : MonoBehaviour {
+
+	public float playerPos;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 thisPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        //if (mousePos.x < transform.position.x) {
+        if (mousePos.x < thisPos.x)
+        {
+
+            GetComponent<SpriteRenderer> ().flipX = true;
+
+		}
+
+		else {
+
+			GetComponent<SpriteRenderer>().flipX = false;
+		}
+
+
+	}
+}
