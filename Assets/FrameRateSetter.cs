044cb13457f40763f323013c84dc173757ff4bb5
natalielh <nlehuenen@gmail.com>
2020-08-04 00:18:14 -0400

    Reduce GPU usage in builds by editing settings

---
+++ Assets/FrameRateSetter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FrameRateSetter : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Make the game run at a certain framerate
+        Application.targetFrameRate = 60;
+        //Application.targetFrameRate = 120;
+
+    }
+
+}
