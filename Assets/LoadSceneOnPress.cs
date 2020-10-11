9aee181ac67a771434430c60d1bc3a04e2fb1ef8
natalielh <nlehuenen@gmail.com>
2020-10-10 23:41:20 -0400

    Add splash screen and lighting fx

--- Assets/LoadSceneOnPress.cs
+++ Assets/LoadSceneOnPress.cs
@@ -6,13 +6,27 @@ using UnityEngine.SceneManagement;
 public class LoadSceneOnPress : MonoBehaviour
 {
 
+    public float delay = 0;
+    private bool keyPressed = false;
+
     public int sceneToLoad;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.anyKeyDown) {
-            SceneManager.LoadScene(sceneToLoad);
+            keyPressed = true;
+            
         }
+
+        if (keyPressed) {
+            delay -= Time.deltaTime;
+            if (delay <= 0)
+            {
+                SceneManager.LoadScene(sceneToLoad);
+            }
+        }
+
+
     }
 }
---
+++ Assets/LookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LookAt : MonoBehaviour
+{
+
+    public Transform transformToLookAt;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.LookAt(transformToLookAt, Vector3.forward);
+
+        Vector2 direction = transformToLookAt.position - transform.position;
+
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = rotation;
+    }
+}
